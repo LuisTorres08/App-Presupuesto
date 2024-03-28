716f1ce4465cc34faeead94e4b94e1fe153735c8
Luis Torres <luisdeavila010@gmail.com>
2024-03-28 12:27:20 -0500

    enter expense validations 2

--- src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
+++ src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PresupuestoService } from 'src/app/services/presupuesto.service';
 
 @Component({
   selector: 'app-ingresar-gasto',
@@ -12,17 +13,35 @@ export class IngresarGastoComponent {
   formularioIncorrecto: boolean;
   textIncorrecto: string;
 
-  constructor() {
+  constructor(private _presupuestoService: PresupuestoService) {
     this.nombreGasto = '';
     this.cantidad = 0;
     this.formularioIncorrecto = false;
-    this.textIncorrecto = 'Nombre gasto o cantidad incorrecta';
+    this.textIncorrecto = '';
   }
 
   agregarGasto() {
+
+    if (this.cantidad > this._presupuestoService.restante) {
+      this.formularioIncorrecto = true;
+      this.textIncorrecto = 'Cantidad ingresada es mayor al restante';
+      return;  
+    }
     if (this.nombreGasto === '' || this.cantidad <= 0) {
       this.formularioIncorrecto = true;
+      this.textIncorrecto = 'Nombre gasto o cantidad incorrecta';
     } else {
+
+      // Creación del objeto
+      const GASTO = {
+        nombre: this.nombreGasto,
+        cantidad: this.cantidad
+      }
+
+      // Enviar el objeto a los subscriptores via subjet
+      this._presupuestoService.agregarGasto(GASTO);
+
+      // Resetear el formulario
       this.formularioIncorrecto = false;
       this.nombreGasto = '';
       this.cantidad = 0;
